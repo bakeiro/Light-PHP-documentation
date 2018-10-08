45faa95384e335008ed06bbf043512876fab5f07
David Baqueiro Santerbas <davidbaqueiro@outlook.com>
2018-10-08 13:54:57 +0200

    Change folder name

---
+++ src/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+	base: 'Light-PHP-documentation',
+	dest: 'vuepress',
+	title: 'Light-PHP',
+	description: 'Light-PHP documentation, Micro framework PHP',
+
+	themeConfig: {
+
+		nav: [
+		  	{ text: 'Home', link: '/' },
+		  	{ text: 'Github', link: 'https://github.com/bakeiro/Light-PHP' },
+			{ text: 'About me', link: 'https://davidbaqueiro.com' },
+		],
+
+		displayAllHeaders: true,
+		sidebar: [
+			{
+				title: 'Introduction',
+				collapsable: false,
+				children: [
+					['/overview/Overview', 'Overview'],
+					['/overview/Configuration', 'Configuration'],
+					['/overview/Routes', 'Routes'],
+					['/overview/Template', 'Template'],
+					['/overview/MoreInfo', 'More info']
+				]
+			},
+			{
+				title: 'Engine',
+				children: [
+					['/engine/engine_structure', 'Structure'],
+					['/engine/folder_structure', 'Folders'],
+					['/engine/database.md', 'Database']
+			  	]
+			}
+		]
+	}
+}
